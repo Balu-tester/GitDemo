305fdb8fae3fd74b15429d79cf33c49241df8bc7
Balu <balur.5b6@gmail.com>
2021-06-25 19:01:01 +0530

    This is My First commit

---
+++ src/main/java/MavenJava/AppiumTest.java
@@ -0,0 +1,20 @@
+package MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NavigateAndriod()
+	{
+		System.out.println("NavigateAndriod");
+	}
+	
+	@Test
+	public void NavigateIOS()
+	
+	{
+		System.out.println("NavigateIOS");
+	}
+
+}
